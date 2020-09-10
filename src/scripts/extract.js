c04add42abe6e9248e1c78e91729d466e42d6728
komadesu <happykoma777@gmail.com>
2020-09-10 21:07:20 +0900

    fix extract.js and confirm whether extract function is working, it became totally good!

--- src/scripts/extract.js
+++ src/scripts/extract.js
@@ -1,66 +1,66 @@
 window.addEventListener('load', () => {
-  let bases = document.querySelectorAll('.extract__base');
+  let cocktails = document.querySelectorAll('.cocktail__item');
   $('.extract-option').on('click', function () {
     if ($('select').val() === 'liquor') {
-      bases.forEach((base) => {
-        if (base.classList.contains('liquor')) {
-          base.style.display = 'block';
+      cocktails.forEach((cocktail) => {
+        if (cocktail.classList.contains('liquor')) {
+          cocktail.style.display = 'block';
         } else {
-          base.style.display = 'none';
+          cocktail.style.display = 'none';
         }
       });
     }
     if ($('select').val() === 'gin') {
-      bases.forEach((base) => {
-        if (base.classList.contains('gin')) {
-          base.style.display = 'block';
+      cocktails.forEach((cocktail) => {
+        if (cocktail.classList.contains('gin')) {
+          cocktail.style.display = 'block';
         } else {
-          base.style.display = 'none';
+          cocktail.style.display = 'none';
         }
       });
     }
     if ($('select').val() === 'rum') {
-      bases.forEach((base) => {
-        if (base.classList.contains('rum')) {
-          base.style.display = 'block';
+      cocktails.forEach((cocktail) => {
+        if (cocktail.classList.contains('rum')) {
+          cocktail.style.display = 'block';
         } else {
-          base.style.display = 'none';
+          cocktail.style.display = 'none';
         }
       });
     }
     if ($('select').val() === 'vodka') {
-      bases.forEach((base) => {
-        if (base.classList.contains('vodka')) {
-          base.style.display = 'block';
+      cocktails.forEach((cocktail) => {
+        if (cocktail.classList.contains('vodka')) {
+          cocktail.style.display = 'block';
         } else {
-          base.style.display = 'none';
+          cocktail.style.display = 'none';
         }
       });
     }
     if ($('select').val() === 'tequila') {
-      bases.forEach((base) => {
-        if (base.classList.contains('tequila')) {
-          base.style.display = 'block';
+      cocktails.forEach((cocktail) => {
+        if (cocktail.classList.contains('tequila')) {
+          cocktail.style.display = 'block';
         } else {
-          base.style.display = 'none';
+          cocktail.style.display = 'none';
         }
       });
     }
     if ($('select').val() === 'other') {
-      bases.forEach((base) => {
-        if (base.classList.contains('other')) {
-          base.style.display = 'block';
+      cocktails.forEach((cocktail) => {
+        if (cocktail.classList.contains('other')) {
+          cocktail.style.display = 'block';
         } else {
-          base.style.display = 'none';
+          cocktail.style.display = 'none';
         }
       });
     }
     if ($('select').val() === 'all') {
-      bases.forEach((base) => {
-        if (base.classList.contains('extract__base')) {
-          base.style.display = 'block';
+      cocktails.forEach((cocktail) => {
+        if (cocktail.classList.contains('cocktail__item')) {
+          cocktail.style.display = 'block';
         } else {
-          base.style.display = 'none';
+          cocktail.style.display = 'none';
         }
       });
     }
