0809741cf0665305c61280f5d71ec70396efc116
komadesu <happykoma777@gmail.com>
2020-09-14 18:11:07 +0900

    implement controlling appearance of select menu and separate as ES module

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,10 @@
 import PreviewImage from './preview-image.js';
+import AppearBaseSelect from './appear-base-select.js';
 
 document.addEventListener('DOMContentLoaded', () => {
+
+  const io = new AppearBaseSelect();
+
   const fileInput = document.querySelector('.js-recipe-img-input');
-  const io = new PreviewImage(fileInput);
-})
+  const io2 = new PreviewImage(fileInput);
+});
